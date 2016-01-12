57fe6f69084a0c63cccfed124f309239d299b1d8
Ben Keller <irods.ben.keller@gmail.com>
2016-01-12 16:18:18 -0500

    Use new deploy() interface from federation changes

--- test.py
+++ test.py
@@ -9,7 +9,10 @@ import library
 def main(zone_bundle, deployment_name, packages_root_directory, output_directory, python_client_git_repository, python_client_git_commitish):
     logger = logging.getLogger(__name__)
     zone_bundle_output_file = os.path.join(output_directory, 'deployed_zone.json')
-    deployed_zone_bundle = library.deploy(zone_bundle, deployment_name, packages_root_directory, zone_bundle_output_file)
+    version_to_packages_map = {
+        'deployment-determined': packages_root_directory,
+    }
+    deployed_zone_bundle = library.deploy(zone_bundle, deployment_name, version_to_packages_map, zone_bundle_output_file)
     with library.deployed_zone_bundle_manager(deployed_zone_bundle):
         icat_ip = deployed_zone_bundle['zones'][0]['icat_server']['deployment_information']['ip_address']
 
@@ -20,7 +23,6 @@ def main(zone_bundle, deployment_name, packages_root_directory, output_directory
         }
 
         data = library.run_ansible(module_name='irods_test_python_client', complex_args=complex_args, host_list=[icat_ip])
-        logger.info(library.format_ansible_output(data))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Test Jargon')
